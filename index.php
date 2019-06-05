6af86d0128a9bde4f960cacf62f3c0a3b6d98665
Alunus94 <Alunus94@gmail.com>
2019-06-05 21:53:41 +0300

    Job's done

--- index.php
+++ index.php
@@ -1,7 +1,22 @@
 <?php
 require_once('helpers.php');
 require_once ('functions.php');
-
+function url ($id){
+    $params =$_GET;
+    $params['project_id']=$id;
+    $scriptname=pathinfo(__FILE__,PATHINFO_BASENAME);
+    $query=http_build_query($params);
+    $url ="/".$scriptname."?".$query;
+    return $url;
+}
+function whatever($array,$val) {
+    foreach ($array as $item)
+        if (isset($item['id']) && $item['id'] == $val)
+            return true;
+        elseif (!isset($_GET['project_id']))
+            return true;
+    return false;
+}
 $connect = mysqli_connect("localhost", "root", "","doingsdone");
 mysqli_set_charset($connect, "utf8");
 if ($connect == false) {
@@ -10,15 +25,27 @@ if ($connect == false) {
 }
 else {
     $task= ['1'];
-    $sql = 'SELECT `name`,`date_end`,`date_create`,`status`,`file` FROM tasks where author_id = ?';
-    $tasks_list = db_fetch_data ($connect, $sql, $task);
     $project= ['1'];
-    $sql = 'SELECT p.name, COUNT(project_id) project_count FROM tasks t
-            right JOIN  projects p ON t.project_id = p.id
-            where p.author_id = ?
-            GROUP BY name ORDER BY project_count DESC;';
+    if (isset($_GET['project_id'])){
+        $task= ['1',$_GET['project_id']];
+        $sql = 'SELECT `name`,`date_end`,`date_create`,`status`,`file`,`project_id` FROM tasks where author_id = ? and project_id= ?';
+    }
+        else{
+        $sql = 'SELECT `name`,`date_end`,`date_create`,`status`,`file`,`project_id` FROM tasks where author_id = ?';
+        }
+    $tasks_list = db_fetch_data ($connect, $sql, $task);
+    $sql = 'select p.name, p.id, count(project_id) project_count from tasks t
+            right outer join projects p ON p.id = t.project_id
+            where p.author_id=?
+            group by p.id order by project_count DESC;';
     $projects = db_fetch_data ($connect, $sql, $project);
+        //вывод контента
+    $test =$_GET['project_id'];
+if (whatever($projects,$test)==true) :
     $page_content = include_template('index.php', ['task_list' => $tasks_list]);
+    else:
+        header("Location: 404");
+endif;
     $layout_content = include_template('layout.php', [
         'content' => $page_content,
         'projects' => $projects,
